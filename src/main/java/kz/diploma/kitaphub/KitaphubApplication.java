58b24366a89d500b6826a8b685b1bf2acb0759ea
muratali <amangalimuratali@gmail.com>
2025-04-22 01:43:45 +0500

    fix some error in deploy

--- src/main/java/kz/diploma/kitaphub/KitaphubApplication.java
+++ src/main/java/kz/diploma/kitaphub/KitaphubApplication.java
@@ -5,18 +5,15 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(
-    title = "KitapHub api definition",
-    contact = @Contact(
-        name = "Muratali, Alikhan and Kairkeldi",
-        email = "amangalimuratali@gmail.com"
-    )
-))
+//@OpenAPIDefinition(info = @Info(
+//    title = "KitapHub api definition",
+//    contact = @Contact(
+//        name = "Muratali, Alikhan and Kairkeldi",
+//        email = "amangalimuratali@gmail.com"
+//    )
+//))
 public class KitaphubApplication {
   public static void main(String[] args) {
     SpringApplication.run(KitaphubApplication.class, args);
